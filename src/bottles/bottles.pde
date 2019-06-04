e4b4483e1287408e209693c0ec09a931c7a0cae2
League Student <league@iMac-8.attlocal.net>
2019-06-03 20:02:38 -0700

    //

---
+++ src/bottles/bottles.pde
@@ -0,0 +1,7 @@
+
+for(int i = 99; i > 0; i -- ){
+ System.out.println(i + " bottles of beer on the wall," + i + " bottles of beer.Take one down and pass it around,"+ (i - 1) + " bottles of beer on the wall.");
+ if(i = 0; 
+ //
+}
+ 
--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -3,6 +3,8 @@
 
 package modulo;
 
+import javax.swing.JOptionPane;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
 
 /**
 * Fizz Buzz
@@ -26,6 +28,21 @@ package modulo;
 **/
 
 public class FizzBuzz {
+	public static void main(String[] args){ 
+	
+	for(int i = 1; i < 21; i++) {
+	if(i % 3 == 0 && i % 5 == 0)
+		System.out.println("fizzbuzz");
+		else if(i % 3 == 0)	{
+		System.out.println("fizz");
+	}
+	else if  (i % 5 == 0) {
+		System.out.println("buzz");
+	}
+	else {
+		System.out.println(i);
+	}
+	}
 
 }
-
+}
